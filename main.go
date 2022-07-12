24d0f5511f1e68f5ef7d075b1787c1db653f52bd
liuyi <463455256@qq.com>
2022-07-12 16:50:07 +0800

    前缀树路由

--- main.go
+++ main.go
@@ -9,11 +9,24 @@ func main() {
 	r := jugg.New()
 
 	r.GET("/", func(ctx *jugg.Context) {
-		ctx.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		ctx.HTML(http.StatusOK, "<h1>Hello jugg</h1>")
 	})
 	r.GET("/hello", func(c *jugg.Context) {
 		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
+	r.GET("/hello/:name", func(c *jugg.Context) {
+		// expect /hello/geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+	r.GET("/hello/:name/hello", func(c *jugg.Context) {
+		// expect /hello/geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *jugg.Context) {
+		c.JSON(http.StatusOK, jugg.H{"filepath": c.Param("filepath")})
+	})
+
 	r.POST("/login", func(c *jugg.Context) {
 		c.JSON(http.StatusOK, jugg.H{
 			"username": c.PostForm("username"),
